73d09eae0aa4416916f51e07751afee6001a96d0
jiashun <jiashun@outlook.com>
2018-09-18 21:36:57 +0800

    GoF设计模式

--- src/main/java/org/js/gof/GofApplication.java
+++ src/main/java/org/js/gof/GofApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Author: JiaShun
- * @Date: 2018/9/3 22:01
+ * @author JiaShun
+ * @date 2018/9/3 22:01
  */
 @SpringBootApplication
 public class GofApplication {
